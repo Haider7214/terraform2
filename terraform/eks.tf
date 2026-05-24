5f65e34c8276dbbe78535866081d2e5eadb0bc4c
Haider7214 <99068608+Haider7214@users.noreply.github.com>
2026-05-24 11:19:56 +0530

    Update eks.tf

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -32,7 +32,7 @@ module "eks" {
       min_size     = 1
       max_size     = 2
       desired_size = 1
-      instance_types = ["t2.medium"]
+      instance_types = ["t3.small"]
       capacity_type  = "SPOT"
       tags = {
         ExtraTag = "helloworld"
